a0860db503611c7119d369c04d6ff8a60de81e02
SuepaC <susan.afful@yahoo.com>
2023-07-18 23:36:26 +0000

    Updated Development Branch

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import OutlinedButton from "./assets/Components/Buttons/OutlinedButton";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="text-xl font-Thin">Hello world</h1>
       <PrimaryButton buttonText={"Hi there"} />
       <OutlinedButton buttonText={"Driver"}/>
     </div>
