196b5b9dfdb8fd59c615ca29eeb19ec6e90c6523
Eugene Dumoga <eugenedumoga@gmail.com>
2023-07-24 02:57:13 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import "./index.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import CarOwnersPage from "./pages/CarOwnersPage/CarOwnersPage";
 
 export default function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/carowners" element={<CarOwnersPage />} />
+      </Routes>
     </div>
   );
 }
