ce64e67c91539311b82196eea73dfe3644602a00
Eugene Dumoga <eugenedumoga@gmail.com>
2023-07-24 02:44:23 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import "./index.css";
 import HomePage from "./pages/HomePage/HomePage";
-import CarOwnersPage from "./pages/CarOwnersPage/CarOwnersPage";
 
 export default function App() {
-  return <div>
-    <HomePage />
-    <CarOwnersPage />
-  </div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
