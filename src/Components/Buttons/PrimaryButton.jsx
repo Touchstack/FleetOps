65d945c5f8af129bebc0c8a6ced43bb4c4b7516e
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2024-04-12 18:45:11 +0000

    New login UI completed

--- src/Components/Buttons/PrimaryButton.jsx
+++ src/Components/Buttons/PrimaryButton.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
-const PrimaryButton = ({ buttonText, onClick }) => {
+const PrimaryButton = ({ buttonText, onClick, ClassName }) => {
   return (
     <button
       type="button"
-      className="text-white text-center bg-fleetBlue font-Regular text-lg px-10 pt-3 pb-2 mr-4 rounded-[7px] hover:bg-fleetLightBlue"
+      className={`text-white text-center bg-fleetBlue font-Regular text-lg px-10 pt-3 pb-2 mr-4 rounded-[7px] hover:bg-fleetLightBlue ${ClassName}`}
       onClick={onClick}
     >
       {buttonText}
@@ -15,11 +15,13 @@ const PrimaryButton = ({ buttonText, onClick }) => {
 PrimaryButton.propTypes = {
   buttonText: PropTypes.string,
   onClick: PropTypes.func,
+  ClassName: PropTypes.string
 };
 
 PrimaryButton.defaultProps = {
   buttonText: "",
   onClick: () => {},
+  ClassName: ""
 };
 
 export default PrimaryButton;
--- src/pages/SignIn/LoginPage.jsx
+++ src/pages/SignIn/LoginPage.jsx
@@ -1,55 +1,9 @@
-import { useState, useEffect } from "react";
-import * as Yup from "yup";
-import {
-  Spinner,
-  ErrorAlert,
-} from "../../Components/Forms/CarOwnersRegistrationForm";
-import { apiDriverLogin } from "../../services/VehiclesService";
-import FormInputItem from "../../Components/Forms/Inputs/RegistrationFormInput";
 import DriversOnboardingNavBar from "../../Components/Navbar/DriversOnboardingNavBar";
-import PrimaryButton from "../../Components/Buttons/PrimaryButton";
-import { Formik } from "formik";
-import { useNavigate } from "react-router-dom";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/Components/ui/tabs";
+import DriverLogin from "./DriverLogin";
+import CarOwnerLogin from "./CarOwnerLogin";
 
 const LoginPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [errorText, setErrorText] = useState("");
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      return navigate("/drivers/dashboard");
-    }
-  }, []);
-
-  const validation = Yup.object().shape({
-    phoneNumber: Yup.string()
-      .matches(
-        /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
-        "Phone Number is Invalid"
-      )
-      .required("Phone Number is Required"),
-  });
-
-  const registerDriver = async (values) => {
-    try {
-      setError(false);
-      setLoading(true);
-      const response = await apiDriverLogin(values);
-      const userData = response.data?.data;
-      localStorage.setItem("driver", JSON.stringify(userData));
-      localStorage.setItem("tempToken", userData?.token);
-      setLoading(false);
-      return (window.location.href = "/otppage");
-    } catch (error) {
-      setLoading(false);
-      setError(true);
-      setErrorText(error?.response?.data?.message || error?.message);
-      setTimeout(() => setError(false), 3000);
-    }
-  };
 
   return (
     <div className="bg-[#F7F9F8] min-h-screen">
@@ -64,41 +18,37 @@ const LoginPage = () => {
         <h3 className="font-Bold text-[#0A0D14] text-center lg:text-5xl md:text-4xl sm:text-4xl text-4xl lg:w-4/12 md:w-8/12 sm:w-11/12 w-11/12">
           Welcome Back
         </h3>
-        <h3 className="text-[#666666] font-Light">Login To Continue</h3>
-        <Formik
-          initialValues={{ phoneNumber: "" }}
-          validationSchema={validation}
-          onSubmit={(values) => registerDriver(values)}
-        >
-          {({ handleChange, handleSubmit, touched, errors, values }) => (
-            <div className="bg-[#F7F9F8] mt-5">
-              {error && <ErrorAlert error={errorText} />}
-              <label htmlFor="phone" className="font-Regular">
-                Enter your number
-              </label>
+        <h3 className="text-[#666666] font-Light pb-5">Login To Continue</h3>
+        
 
-              <FormInputItem
-                placeholder={"e.g  0245869979"}
-                type={"tel"}
-                id={"phonenumber"}
-                value={values.phoneNumber}
-                onChange={handleChange("phoneNumber")}
-              />
-              {touched.phoneNumber && errors.phoneNumber && (
-                <p className="text-red-500 underline font-Regular pb-2">
-                  {errors.phoneNumber}
-                </p>
-              )}
-              <div className="mt-10 flex justify-end align-baseline">
-                {loading ? (
-                  <Spinner />
-                ) : (
-                  <PrimaryButton buttonText={"Next"} onClick={handleSubmit} />
-                )}
-              </div>
-            </div>
-          )}
-        </Formik>
+
+      {/* Tabs */}
+       <Tabs defaultValue="Driver Login" className="mr-auto z-0 w-full">
+        <TabsList className="w-full justify-center rounded-none bg-transparent pl-0">
+          <TabsTrigger
+            value="Driver Login"
+            className="relative rounded-none border-b-2  bg-transparent md:px-10 px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-[#234C65] data-[state=active]:text-[#234C65] data-[state=inactive]:text-[#ABB3BF] data-[state=active]:shadow-none "
+          >
+           Driver Login 
+          </TabsTrigger>
+          <TabsTrigger
+            value="Car Owner Login"
+            className="relative rounded-none border-b-2  bg-transparent md:px-10 px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-[#234C65] data-[state=active]:text-[#234C65] data-[state=inactive]:text-[#ABB3BF] data-[state=active]:shadow-none "
+          >
+            Car Owner Login
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="Driver Login">
+            <DriverLogin />
+        </TabsContent>
+
+        <TabsContent value="Car Owner Login">
+            <CarOwnerLogin />
+        </TabsContent>
+
+       </Tabs>
+
       </div>
     </div>
   );
